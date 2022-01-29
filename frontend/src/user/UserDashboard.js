483b59c57a2e7e10199f2c33dc2038d5843c1a7c
Haradeep2002 <mh322.mara@gmail.com>
2022-01-30 02:49:09 +0530

    Reviews,Ratings of products to be added

--- frontend/src/user/UserDashboard.js
+++ frontend/src/user/UserDashboard.js
@@ -11,6 +11,7 @@ const Dashboard = () => {
 
     const init = (userId,token) => {
         getPurchaseHistory(userId,token).then(data => {
+            console.log(data)
             if(data.error)console.log(data.error)
             else setHistory(data)
         })
@@ -54,6 +55,7 @@ const Dashboard = () => {
             <h3 className="card-header">Purchase history</h3>
             <ul className="list-group">
                 <li className="list-group-item">
+                    {console.log(history)}
                     {history.map((h, i) => {
                         return (
                             <div>
@@ -62,15 +64,19 @@ const Dashboard = () => {
                                     return (
                                         <div key={i}>
                                             <h6>Product name: {p.name}</h6>
-                                            <h6>Product price: ${p.price}</h6>
+                                            <h6>Product price: Rs.{p.price}</h6>
+                                            <h6>Product Quantity: Rs.{p.count}</h6>
                                             <h6>
                                                 Purchased date:{" "}
                                                 {moment(p.createdAt).fromNow()}
                                             </h6>
+                                            <br></br>
                                             {console.log(p)}
                                         </div>
                                     );
                                 })}
+                                <h6 className='badge btn-warning'>Total amount: Rs.{h.amount}</h6><br></br>
+                                <h6 className='badge btn-info'>Status: {h.status}</h6>
                             </div>
                         );
                     })}
