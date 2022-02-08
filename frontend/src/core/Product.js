5a36607a7ce47fb51c8806e38e4c31f5c9354c25
krishna-chitlangi <67702087+krishna-chitlangi@users.noreply.github.com>
2022-02-08 12:20:38 +0530

    bug cleared

--- frontend/src/core/Product.js
+++ frontend/src/core/Product.js
@@ -1,10 +1,10 @@
-import React,{ useState,useEffect } from "react";
-import { getProducts,read } from "./apiCore";
+import React, { useState, useEffect } from "react";
+import { getProducts, read } from "./apiCore";
 import Layout from "./Layout";
 import Card from "./Card";
 import { listRelated } from "./apiCore";
 
-const Product = (props) =>{
+const Product = (props) => {
     const [product, setProduct] = useState({})
     const [relatedProduct, setRelatedProduct] = useState([])
     const [error, setError] = useState(false)
@@ -12,14 +12,14 @@ const Product = (props) =>{
         // console.log(id)
         //do async-await
         read(id).then(data => {
-            if(data.error){
+            if (data.error) {
                 setError(data.error)
-            }else{
+            } else {
                 setProduct(data)
                 listRelated(data._id).then(res => {
-                    if(res.error){
+                    if (res.error) {
                         setError(res.error)
-                    }else{
+                    } else {
                         setRelatedProduct(res)
                     }
                 })
@@ -30,16 +30,16 @@ const Product = (props) =>{
     useEffect(() => {
         const id = props.match.params.id
         loadSingleProduct(id)
-    },[props])
-    return(
-        <Layout title={product && product.name} description={product && product.description && product.description.substring(0,100)} className='container-fluid'>
-          <div className="row">
-                <div className="col-6" style={{paddingRight:'50px'}}>
-                    {product && product.description && <Card product={product} showViewProductButton={false} showDescription={true}/>}
+    }, [props])
+    return (
+        <Layout title={product && product.name} description={product && product.description && product.description.substring(0, 100)} className='container-fluid'>
+            <div className="row">
+                <div className="col-6" style={{ paddingRight: '50px' }}>
+                    {product && product.description && <Card product={product} showViewProductButton={false} showDescription={true} />}
                 </div>
-<div className="vr"></div>
-                <div className="col-5" style={{paddingLeft:'50px'}}>
-                    <h4>Related products</h4>
+                <div className="vr"></div>
+                <div className="col-5" style={{ paddingLeft: '50px' }}>
+                    <h4 style={{ backgroundColor: '#001233', color: '#EFE0CA', padding: '10px', marginBottom: '30px' }}>Related products</h4>
                     {relatedProduct.map((p, i) => (
                         <div className="mb-3" key={i}>
                             <Card product={p} />
@@ -48,7 +48,7 @@ const Product = (props) =>{
                 </div>
             </div>
         </Layout>
-      )
+    )
 }
 
 export default Product
